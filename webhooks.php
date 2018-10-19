9cd7b465468ed0838db9ad5d52d65e483c1ca327
ImmaGOOD <42084689+ImmaGOOD@users.noreply.github.com>
2018-10-19 16:32:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -19,7 +19,7 @@ $events = json_decode($content, true);
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $event['message']['text']
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
