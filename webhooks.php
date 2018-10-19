06973e3c2c25c6ef2818464f15980e4120a3cbe9
ImmaGOOD <42084689+ImmaGOOD@users.noreply.github.com>
2018-10-19 15:40:09 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -52,8 +52,8 @@ if (!is_null($events['events'])) {
 
 echo "OK";
 <?php
-    $accessToken = 'PPv2psVIO166J7ouwWrXVHVDBSpJmlajX6E2WIKHw0aXOGnZD1+UFLbXlfk5t9YV56hLZk3JvxIM4GBf7U3jExNolDBVRDk/2kXLsV6Wda5Pj4ecjRoR6vpSJ8Vp7q6KQLBDhEaVyaqtlrkqPJlAMwdB04t89/1O/w1cDnyilFU=
-';
+    $accessToken = "PPv2psVIO166J7ouwWrXVHVDBSpJmlajX6E2WIKHw0aXOGnZD1+UFLbXlfk5t9YV56hLZk3JvxIM4GBf7U3jExNolDBVRDk/2kXLsV6Wda5Pj4ecjRoR6vpSJ8Vp7q6KQLBDhEaVyaqtlrkqPJlAMwdB04t89/1O/w1cDnyilFU=
+";
     
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
