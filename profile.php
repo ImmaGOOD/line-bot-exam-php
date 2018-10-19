918c4575a91867b7e078221adc1703fc1eb72490
ImmaGOOD <42084689+ImmaGOOD@users.noreply.github.com>
2018-10-19 15:23:15 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@
 $access_token = 'PPv2psVIO166J7ouwWrXVHVDBSpJmlajX6E2WIKHw0aXOGnZD1+UFLbXlfk5t9YV56hLZk3JvxIM4GBf7U3jExNolDBVRDk/2kXLsV6Wda5Pj4ecjRoR6vpSJ8Vp7q6KQLBDhEaVyaqtlrkqPJlAMwdB04t89/1O/w1cDnyilFU=
 ';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Uc0a3582697fe7dd5aa58bb397fe252fe';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
