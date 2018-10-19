82298a52c15ba8f692994ed8eaca8277563b464e
ImmaGOOD <42084689+ImmaGOOD@users.noreply.github.com>
2018-10-19 17:04:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -61,10 +61,21 @@ $access_token = "PPv2psVIO166J7ouwWrXVHVDBSpJmlajX6E2WIKHw0aXOGnZD1+UFLbXlfk5t9Y
 	$channelSecret = 'a5966438c02bc014296c5ca5b43b5bc4';
 $idPush = $id;
 	
+	$messages = [
+				'type' => 'image',
+				'originalContentUrl' => 'https://www.w3schools.com/w3css/img_lights.jpg',
+				'previewImageUrl'  => 'https://www.w3schools.com/w3css/img_lights.jpg'
+			];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
 	
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($data);
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
